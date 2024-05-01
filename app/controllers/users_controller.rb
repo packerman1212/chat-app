1638449b79a8803f221b7aee8e4cd4ff3b839e26
packerman1212 <mezase226334@gmail.com>
2024-05-01 17:21:46 +0900

    ユーザー編集機能の実装・修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     if current_user.update(user_params)
       redirect_to root_path
     else
-      rander :edit, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
